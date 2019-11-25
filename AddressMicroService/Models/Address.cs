6fcabca70d6b051ca382f1a3c4ccc0dfa7dae472
Ross Wilson <rosswilson1@me.com>
2019-11-25 14:50:41 +0000

    Adding Model for address

---
+++ AddressMicroService/Models/Address.cs
@@ -0,0 +1,11 @@
+﻿namespace AddressMicroService.Models
+{
+    public class Address
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string StreetAddress { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+    }
+}
