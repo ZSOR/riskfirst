75f57b3b632494a9f8f8ccd098d92380d05d315a
Ross Wilson <rosswilson1@me.com>
2019-11-25 15:31:26 +0000

    Adding repo structure and an id to address so they can be easily referenced

---
+++ AddressMicroService/Repositories/AddressRepository.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AddressMicroService.Models;
+
+namespace AddressMicroService.Repositories
+{
+    public class AddressRepository : IAddressRepository
+    {
+        public void DeleteAddress()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Address GetAddress(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Address> GetAddresses()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void InsertAddress(Address address)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateAddress(Address address)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ AddressMicroService/Repositories/IAddressRepository.cs
@@ -0,0 +1,19 @@
+﻿using AddressMicroService.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AddressMicroService.Repositories
+{
+    public interface IAddressRepository
+    {
+        IEnumerable<Address> GetAddresses();
+        Address GetAddress(int id);
+        void InsertAddress(Address address);
+        void DeleteAddress();
+        void UpdateAddress(Address address);
+        void Save();
+
+    }
+}
