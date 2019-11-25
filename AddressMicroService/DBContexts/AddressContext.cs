64a635f14527ea36f69035c50fb9148ece4bda39
Ross Wilson <rosswilson1@me.com>
2019-11-25 15:19:35 +0000

    Adding DB context and local sqlexpress database

---
+++ AddressMicroService/DBContexts/AddressContext.cs
@@ -0,0 +1,41 @@
+﻿using AddressMicroService.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AddressMicroService.DBContexts
+{
+    public class AddressContext : DbContext
+    {
+        public AddressContext(DbContextOptions<AddressContext> options) : base(options) { }
+
+        public DbSet<Address> Addresses { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Address>().HasData(
+                new Address
+                {
+                    FirstName = "John",
+                    LastName = "Smith",
+                    StreetAddress = "Test St 1",
+                    City = "London",
+                    Country = "England",
+                },
+                new Address
+                {
+                    FirstName = "Jane",
+                    LastName = "Doe",
+                    StreetAddress = "Test St 2",
+                    City = "london",
+                    Country = "England",
+                },
+                new Address
+                {
+                    FirstName = "Tim",
+                    LastName = "Jones",
+                    StreetAddress = "Test St 3",
+                    City = "New York",
+                    Country = "USA",
+                });
+        }
+    }
+}
--- AddressMicroService/Startup.cs
+++ AddressMicroService/Startup.cs
@@ -2,10 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AddressMicroService.DBContexts;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -26,6 +28,7 @@ namespace AddressMicroService
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddDbContext<AddressContext>(x => x.UseSqlServer(Configuration.GetConnectionString("DevelopDB")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
