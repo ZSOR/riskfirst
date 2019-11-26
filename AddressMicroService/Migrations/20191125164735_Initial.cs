ce18aed970424e660ffae5b3ab5fc27723d989d2
Ross Wilson <rosswilson1@me.com>
2019-11-26 19:20:26 +0000

    Adding migrations

---
+++ AddressMicroService/Migrations/20191125164735_Initial.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AddressMicroService.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Addresses",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(nullable: true),
+                    LastName = table.Column<string>(nullable: true),
+                    StreetAddress = table.Column<string>(nullable: true),
+                    City = table.Column<string>(nullable: true),
+                    Country = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Addresses", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Addresses",
+                columns: new[] { "Id", "City", "Country", "FirstName", "LastName", "StreetAddress" },
+                values: new object[] { 1, "London", "England", "John", "Smith", "Test St 1" });
+
+            migrationBuilder.InsertData(
+                table: "Addresses",
+                columns: new[] { "Id", "City", "Country", "FirstName", "LastName", "StreetAddress" },
+                values: new object[] { 2, "london", "England", "Jane", "Doe", "Test St 2" });
+
+            migrationBuilder.InsertData(
+                table: "Addresses",
+                columns: new[] { "Id", "City", "Country", "FirstName", "LastName", "StreetAddress" },
+                values: new object[] { 3, "New York", "USA", "Tim", "Jones", "Test St 3" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Addresses");
+        }
+    }
+}
